baa38548241e6af687e11e9b48b7203573cc9b34
Oxitocina <isidoro.goniri@gmail.com>
2014-11-27 12:46:40 +0100

    correcciones grafico

--- reversi-grafico.py
+++ reversi-grafico.py
@@ -1,16 +1,19 @@
 from Tablero import *
 from graphics import *
+from copy import deepcopy
 class Partida():
 	
 	def __init__(self):
 		self.ventana = GraphWin("Reversi", 555, 555)
 		self.tablero = Image(Point(277.5,277.5), "tablero-ajedrez.png")
 		self.tabl = Tablero()
-		self.fichas = []
-	def main():
-		tablero.draw(ventana)
-		ventana.getMouse() 
-		ventana.close()
+		self.fichas = 4
+		self.main()
+	def main(self):
+		self.tablero.draw(self.ventana)
+		self.coloca_fichas()
+		self.jugar()
+		#self.ventana.close()
 
 	def coloca_fichas(self):
 		for i in self.tabl.tablero:
@@ -18,14 +21,17 @@ class Partida():
 				if x == 1:
 					circulo = Circle(Point((i+1)*69-34.5, (j+1)*69-34.5), 34.5)
 					circulo.setFill("red")
-					self.fichas.append(deepcopy(circulo)) #Deep copy?
+					circulo.draw(self.ventana)
 				elif x == -1:
 					circulo = Circle(Point((i+1)*69-34.5, (j+1)*69-34.5), 34.5)
 					circulo.setFill("green")
-					self.fichas.append(deepcopy(circulo)) #Deep copy?
+					circulo.draw(self.ventana)
 	def jugar(self):
-		while len(self.fichas)<64:
-			jugada = ventana.getMouse()
-			if play(int(jugada.x/69)+1,int(jugada.y/69)+1):
-				self.coloca_fichas()
+		while self.fichas<64:
+			jugada = self.ventana.getMouse()
+			if self.tabl.play(int(jugada.x/69)+1,int(jugada.y/69)+1):
+				self.fichas += 1
+				self.coloca_fichas
 
+if __name__ == "__main__":
+	culo = Partida()
