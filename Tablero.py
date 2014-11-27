10ca85e2bf3c77f0248de9c8f7d51bfe5bd1e72c
Mognom <alex.mognom@gmail.com>
2014-11-27 16:18:25 +0000

    Debug time =D

--- Tablero.py
+++ Tablero.py
@@ -5,125 +5,142 @@ class Tablero:
 	def __init__(self):
 		self.jugador = 1
 		self.tablero=[]
-		fila = []
 		for i in range(8):
-			fila.append(0)
-		for i in range(8):
-			self.tablero.append(fila)
+			self.tablero.append([0,0,0,0,0,0,0,0])
+		self.tablero[3][3]=1
+		self.tablero[4][4]=1
+		self.tablero[3][4]=-1
+		self.tablero[4][3]=-1
+
+	def draw(self):
+		for i in self.tablero:
+			for x in i:
+				print(x, end = " ")
+			print("\n")
 
 	def play (self, x, y):
-		if canPlay(x,y):
-			avalancha(x,y)
-			tablero[x][y] = jugador
-			jugador *= -1
+		if self.canPlay(x,y):
+			print("intenta")
+			self.tablero[x][y] = self.jugador
+			self.jugador *= -1
 
 	#Comprueba si en esa casilla se puede colocar ficha =D
 	def canPlay (self, x, y):
-		if tablero[x][y] == 0:
+		if self.tablero[x][y] == 0:
 			
-			resultado = 0
+			resultado = False
 			#Comprobacion izquierda
 			validMove = False
 			for i in range(x):
-				if tablero[x-i-1][y] == jugador*-1:
+				if self.tablero[x-i-1][y] == self.jugador*-1:
 					validMove = True
-				if tablero[x-i-1][y] == jugador:
+				if self.tablero[x-i-1][y] == self.jugador:
 					break
-				if tablero[x-i-1][y] == 0:
+				if self.tablero[x-i-1][y] == 0:
 					validMove = False
+					break
 			if validMove:
-				swapRow(x,y,x-i-1,y)
+				self.swapRow(x,y,x-i-1,y)
 				resultado =True
 
 			#Comprobacion derecha
 			validMove = False
-			for i in range(8-x):
-				if tablero[x+i+1][y] == jugador*-1:
+			for i in range(8-x-1):
+				if self.tablero[x+i+1][y] == self.jugador*-1:
 					validMove = True
-				if tablero[x+i+1][y] == jugador:
+				if self.tablero[x+i+1][y] == self.jugador:
 					break
-				if tablero[x+i+1][y] == 0:
+				if self.tablero[x+i+1][y] == 0:
 					validMove = False
+					break
 			if validMove:
-				swapRow(x,y,x+i+1,y)
+				self.swapRow(x,y,x+i+1,y)
 				resultado =True
 
 			#Comprobacion arriba
 			validMove = False
 			for i in range(y):
-				if tablero[x][y-i-1] == jugador*-1:
+				if self.tablero[x][y-i-1] == self.jugador*-1:
 					validMove = True
-				if tablero[x][y-i-1] == jugador:
+				if self.tablero[x][y-i-1] == self.jugador:
 					break
-				if tablero[x][y-i-1] == 0:
+				if self.tablero[x][y-i-1] == 0:
 					validMove = False
+					break
 			if validMove:
-				swapRow(x,y,x,y-i-1)
+				self.swapRow(x,y,x,y-i-1)
 				resultado =True
 
 			#Comprobacion abajo
 			validMove = False
-			for i in range(8-y):
-				if tablero[x][y+i+1] == jugador*-1:
+			for i in range(8-y-1):
+				if self.tablero[x][y+i+1] == self.jugador*-1:
 					validMove = True
-				if tablero[x][y+i+1] == jugador:
+				if self.tablero[x][y+i+1] == self.jugador:
 					break
-				if tablero[x][y+i+1] == 0:
+				if self.tablero[x][y+i+1] == 0:
 					validMove = False
+					break
 			if validMove:
-				swapRow(x,y,x,y+i+1)
+				self.swapRow(x,y,x,y+i+1)
 				resultado = True
 
 			#Comprobacion diagonal negativa superior (\)
 			validMove = False
 			for i in range(min(x,y)):
-				if tablero[x-i-1][y-i-1] == jugador*-1:
+				if self.tablero[x-i-1][y-i-1] == self.jugador*-1:
 					validMove = True
-				if tablero[x-i-1][y-i-1] == jugador:
+				if self.tablero[x-i-1][y-i-1] == self.jugador:
 					break
-				if tablero[x-i-1][y-i-1] == 0:
+				if self.tablero[x-i-1][y-i-1] == 0:
 					validMove = False
+					break
 			if validMove:
-				swapRow(x,y,x-i-1,y-i-1)
+				self.swapRow(x,y,x-i-1,y-i-1)
 				resultado =True
 
 			#Comprobacion diagonal negativa inferior (\)
 			validMove = False
-			for i in range(min(8-x,8-y)):
-				if tablero[x+i+1][y+i+1] == jugador*-1:
+			for i in range(min(8-x-1,8-y-1)):
+				if self.tablero[x+i+1][y+i+1] == self.jugador*-1:
 					validMove = True
-				if tablero[x+i+1][y+i+1] == jugador:
+				if self.tablero[x+i+1][y+i+1] == self.jugador:
 					break
-				if tablero[x+i+1][y+i+1] == 0:
+				if self.tablero[x+i+1][y+i+1] == 0:
 					validMove = False
+					break
 			if validMove:
-				swapRow(x,y,x+i+1,y+i+1)
+
+				self.swapRow(x,y,x+i+1,y+i+1)
 				resultado =True
 
 			#Comprobacion diagonal positiva superior (/)
 			validMove = False
-			for i in range(min(8-x,y)):
-				if tablero[x+i+1][y-i-1] == jugador*-1:
+			for i in range(min(8-x-1,y)):
+				if self.tablero[x+i+1][y-i-1] == self.jugador*-1:
 					validMove = True
-				if tablero[x+i+1][y-i-1] == jugador:
+				if self.tablero[x+i+1][y-i-1] == self.jugador:
 					break
-				if tablero[x+i+1][y-i-1] == 0:
+				if self.tablero[x+i+1][y-i-1] == 0:
 					validMove = False
+					break
 			if validMove:
-				swapRow(x,y,x+i+1,y-i-1)
+
+				self.swapRow(x,y,x+i+1,y-i-1)
 				resultado =True
 
 			#Comprobacion diagonal positiva inferior (/)
 			validMove = False
-			for i in range(min(x,8-y)):
-				if tablero[x-i-1][y+i+1] == jugador*-1:
+			for i in range(min(x,8-y-1)):
+				if self.tablero[x-i-1][y+i+1] == self.jugador*-1:
 					validMove = True
-				if tablero[x-i-1][y+i+1] == jugador:
+				if self.tablero[x-i-1][y+i+1] == self.jugador:
 					break
-				if tablero[x-i-1][y+i+1] == 0:
+				if self.tablero[x-i-1][y+i+1] == 0:
 					validMove = False
+					break
 			if validMove:
-				swapRow(x,y,x-i-1,y+i+1)
+				self.swapRow(x,y,x-i-1,y+i+1)
 				resultado =True
 
 			#Si una vez llegado aqui el resultado es 1, el movimiento ha sido valido
@@ -131,5 +148,22 @@ class Tablero:
 	#DEPRECATED
 	def avalancha(self, x, y):
 		pass
-	def swapRow(self, x, y):
+	def swapRow(self, x0, y0, x1, y1):
+		xworth = min(x0,x1)
+		x = max(x0,x1)
+		yworth=min(y0,y1)
+		y=max(y0,y1)
+		while xworth != x or yworth != y:
+			#print(xworth)
+			#print(yworth)
+			self.tablero[xworth][yworth] = self.jugador
+			if xworth != x:
+				xworth+=1
+			if yworth != y:
+				yworth+=1
 		return True
+
+if __name__ == "__main__":
+	culo = Tablero()
+	print(culo.play(5,3))
+	culo.draw()
